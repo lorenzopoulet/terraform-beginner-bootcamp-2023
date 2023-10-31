1bd3eee2d741aea11dcdb6e5d9daac341dd71183
lorenzo poulet <haroon.ahmad@grenoble-em.com>
2023-10-31 15:47:16 +0000

    restructure root module

--- main.tf
+++ main.tf
@@ -1,10 +1,18 @@
-terraform {
-  cloud {
-    organization = "BootcampOrg"
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+resource "random_string" "bucket_name" {
+  lower = true
+  upper = false
+  length   = 32
+  special  = false
+}
 
-    workspaces {
-      name = "terra-house-1"
-      
-    }
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "example" {
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+  bucket = random_string.bucket_name.result
+
+  tags = {
+    UserUuid = var.user_uuid
   }
 }
