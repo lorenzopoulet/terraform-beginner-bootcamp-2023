053319b1919bb0b2ff0861488e507961793f64c8
haroon.af_ <141220777+lorenzopoulet@users.noreply.github.com>
2023-11-01 10:40:51 +0100

    Terrahouse module (#15)

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+output "bucket_name" {
+  description = "Bucket name for our static website hosting"
+  value = module.terrahouse_aws.bucket_name
 }
